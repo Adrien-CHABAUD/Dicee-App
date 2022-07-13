14c2b565783c4202f2de7e847166857c37ce93c9
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-07-13 15:00:56 -0400

    Add randomization of selection of dices.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,39 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    // Store the number of the dice to show and where
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        /*
-         // UIImage allows to load an image.
-         diceImageView1.image = UIImage(named: "DiceOne")
-         diceImageView2.image = UIImage(named: "DiceSix")
-        */
-    }
-    
     // IBAction allows actions to be made after a trigger
     // is made.
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        /*
-         // Change image of dice in each ImageView
-         diceImageView1.image = UIImage(named: "DiceFour")
-         diceImageView2.image = UIImage(named: "DiceFour")
-         */
+        
+        // Array containing all the dice faces
+        let diceArray = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
         
         // Change the image contained in each ImageView by putting the references to the dices in an array
-        diceImageView1.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][leftDiceNumber]
-        
-        diceImageView2.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][rightDiceNumber]
-        
-        
-        
-        // Uncrease the left dice value
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
+        diceImageView1.image = diceArray[Int.random(in: 1...5)]
+        diceImageView2.image = diceArray[Int.random(in: 1...5)]
     }
     
 }
